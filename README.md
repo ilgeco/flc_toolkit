2a01536edf590f8758e25962e6d253861b855fde
ilgeco <gabriele.magnani@mail.polimi.it>
2024-08-26 11:16:43 +0200

    add coverage

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # Formal Languages and Compilers Toolkit
 
 Work in progress!
+
+
+# Coverage
+
+### Example: How to generate source-based coverage for a Rust project
+
+1. Install the llvm-tools or llvm-tools-preview component:
+
+   ```sh
+   rustup component add llvm-tools-preview
+   ```
+
+2. Ensure that the following environment variable is set up:
+
+   ```sh
+   export RUSTFLAGS="-Cinstrument-coverage"
+   ```
+
+3. Build your code:
+
+   `cargo build`
+
+4. Ensure each test runs gets its own profile information by defining the LLVM_PROFILE_FILE environment variable (%p will be replaced by the process ID, and %m by the binary signature):
+
+   ```sh
+   export LLVM_PROFILE_FILE="your_name-%p-%m.profraw"
+   ```
+
+5. Run your tests:
+
+   `cargo test`
+
+In the CWD, you will see a `.profraw` file has been generated. This contains the profiling information that grcov will parse, alongside with your binaries.
+
+Generate a html coverage report like this:
+
+```sh
+grcov . -s ./src/ --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+```
--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,8 @@ pub use crate::elr_pilot::*;
 pub use crate::lexer::*;
 pub use crate::parser::*;
 
-fn main() {
-    let args: Vec<_> = args().collect();
-    if args.len() != 2 {
-        eprintln!("usage: {} [file]", args[0]);
-        exit(1);
-    }
-
-    let lex = Lexer::from_path(Path::new(&args[1]));
+fn generate_pilot(path: impl AsRef<Path>) {
+    let lex = Lexer::from_path(Path::new(path.as_ref()));
     let mut pars = Parser::new(lex);
     if let Some(net) = pars.parse_mnet() {
         if net.validate() {
@@ -27,5 +21,32 @@ fn main() {
             println!("{}", pilot.to_dot());
             pilot.print_conflicts();
         }
+    }
+}
+
+fn main() {
+    let args: Vec<_> = args().collect();
+    if args.len() != 2 {
+        eprintln!("usage: {} [file]", args[0]);
+        exit(1);
+    }
+    generate_pilot(&args[1]);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::generate_pilot;
+
+    #[test]
+    fn test_generate_pilot() {
+        generate_pilot(r"./tests/cursed.txt");
+        generate_pilot(r"./tests/dangling_else.txt");
+        generate_pilot(r"./tests/elr_mnet_2013-02-05.txt");
+        generate_pilot(r"./tests/elr_mnet_2020-01-14.txt");
+        generate_pilot(r"./tests/elr_mnet_2024-02-13.txt");
+        generate_pilot(r"./tests/elr_mnet_2024-06-13.txt");
+        generate_pilot(r"./tests/elr_mnet_2024-07-04.txt");
+        generate_pilot(r"./tests/elr_mnet_book-4.15.txt");
+        generate_pilot(r"./tests/elr_mnet_book-4.16.txt");
     }
 }
